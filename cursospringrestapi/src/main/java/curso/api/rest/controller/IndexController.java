e7b3977e6f7d95eb74f9a28551d2d51262519f6d
Ssxund3r <https://github.com/Ssxund3r>
2023-05-27 14:08:50 -0300

    Retornando usuário JSON para a tela

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,12 +1,17 @@
 package curso.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import curso.api.rest.model.Usuario;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -18,11 +23,24 @@ public class IndexController {
 	//Serviço RESTful
 	@ApiOperation("Obtém uma lista de objetos de exemplo")
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init(@RequestParam (value = "nome", required = true, 
-	defaultValue = "Nome não Informado") String nome, @RequestParam("salario") Long salario)  {
+	public ResponseEntity<Usuario> init()  {
+		Usuario usuario = new  Usuario();
+		usuario.setId(1L);
+		usuario.setLogin("gabriel.fcosta@gmail.com");
+		usuario.setSenha("123");
+		usuario.setNome("Gabriel");
 		
-		System.out.println("Parametro sendo recebido " + nome);
-		return new ResponseEntity("Olá Usuário REST Spring Boot " + nome + " salario é: " + salario, HttpStatus.OK);
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(2L);
+		usuario2.setLogin("natalia.scosta@gmail.com");
+		usuario2.setSenha("456");
+		usuario2.setNome("Natália");
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		
+		return new ResponseEntity(usuarios, HttpStatus.OK);
 	}
 	
 	
