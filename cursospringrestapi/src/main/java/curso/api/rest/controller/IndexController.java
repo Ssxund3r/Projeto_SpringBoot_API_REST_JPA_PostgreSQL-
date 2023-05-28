094e85a51604eb43500e12e41f674c89ed5e60c7
Ssxund3r <https://github.com/Ssxund3r>
2023-05-28 20:21:27 -0300

    Criando atualização de cadastro com o PUT

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,16 +27,16 @@ public class IndexController {
 	@Autowired //Se fosse CDI seria @Inject
 	private UsuarioRepository usuarioRepository;
 	
-	//Serviço RESTful
-		@ApiOperation("Obtém uma lista de objetos de exemplo")
-		@GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
-		public ResponseEntity<Usuario> relatorio(@PathVariable (value = "id") Long id,
-																@PathVariable (value="venda") Long venda){	
-			
-			Optional<Usuario> usuario = usuarioRepository.findById(id);
-			
-			return new ResponseEntity(usuario.get(), HttpStatus.OK);
-		}
+	// Serviço RESTful
+	@ApiOperation("Obtém uma lista de objetos de exemplo")
+	@GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
+	public ResponseEntity<Usuario> venda(@PathVariable(value = "id") Long id,
+											 @PathVariable(value = "venda") Long venda) {
+
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+		return new ResponseEntity(usuario.get(), HttpStatus.OK);
+	}
 	
 	//Serviço RESTful
 	@ApiOperation("Obtém uma lista de objetos de exemplo")
@@ -72,4 +73,20 @@ public class IndexController {
 		return new ResponseEntity("id user :" + iduser + " idvenda :" + idvenda, HttpStatus.OK);
 	}
 	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizarcadastro(@RequestBody Usuario usuario){
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/vendausuario/idvenda/{idvenda}", produces = "application/json")
+	public ResponseEntity<Usuario> updadevenda(@PathVariable Long iduser, 
+											   @PathVariable Long idvenda){
+		
+		/*Aqui seria o processo de venda
+		 *Usuario usuarioSalvo = usuarioRepository.save(usuario);*/
+		
+		return new ResponseEntity("Venda Atualizada", HttpStatus.OK);
+	}
 }
