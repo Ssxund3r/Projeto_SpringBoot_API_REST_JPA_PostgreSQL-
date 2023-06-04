5c16a3dbaaf3dce43ff1e20ff3d8f096d4273886
Ssxund3r <https://github.com/Ssxund3r>
2023-06-04 14:50:19 -0300

    Implementando o END-POINT de atualização e refatorações necessárias

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,9 +1,12 @@
 package curso.api.rest.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,115 +14,98 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
 import curso.api.rest.repository.UsuarioRepository;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import springfox.documentation.swagger2.mappers.ModelMapper;
 
 @RestController //Arquitetura REST
-@Api(value = "Exemplo de API")
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
 	@Autowired //Se fosse CDI seria @Inject
 	private UsuarioRepository usuarioRepository;
 	
-	// Serviço RESTful
-	@ApiOperation("Obtém uma lista de objetos de exemplo")
-	@GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
-	public ResponseEntity<Usuario> venda(@PathVariable(value = "id") Long id,
-											 @PathVariable(value = "venda") Long venda) {
-
-		Optional<Usuario> usuario = usuarioRepository.findById(id);
-
-		return new ResponseEntity(usuario.get(), HttpStatus.OK);
-	}
-	
 	//Serviço RESTful
-	@ApiOperation("Obtém uma lista de objetos de exemplo")
 	@GetMapping(value = "/{id}", produces = "application/json")
-	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id){	
-		
-		Optional<Usuario> usuario = usuarioRepository.findById(id);
-		
-		return new ResponseEntity(usuario.get(), HttpStatus.OK);
+	public ResponseEntity<Usuario> getusuarioporid(@PathVariable Long id) {
+	    return usuarioRepository.findById(id)
+	            .map(usuario -> ResponseEntity.ok(usuario))
+	            .orElse(ResponseEntity.notFound().build());
 	}
 	
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<List<Usuario>> usuario(){
+	public ResponseEntity<List<Usuario>> listarusuarios(){
+		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
 		
-		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
-		
-		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+		return ResponseEntity.ok(usuarios);
 	}
 	
 	@PostMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){	
+		usuario.getTelefones().forEach(telefone -> telefone.setUsuario(usuario));		
+		
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
 	
-	@PostMapping(value = "/vendausuario/idvenda/{idvenda}", produces = "application/json")
-	public ResponseEntity<Usuario> cadastrarvenda(@PathVariable Long iduser, 
-												  @PathVariable Long idvenda){
-		
-		/*Aqui seria o processo de venda
-		 *Usuario usuarioSalvo = usuarioRepository.save(usuario);*/
-		
-		return new ResponseEntity("id user :" + iduser + " idvenda :" + idvenda, HttpStatus.OK);
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizarcadastro(@RequestBody Usuario usuario){		
+		if (usuario.getId() == null) {
+	        return ResponseEntity.badRequest().build();
+	    }
+	    if (!usuarioRepository.existsById(usuario.getId())) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    
+	    for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+	    
+	    Usuario usuarioSalvo = usuarioRepository.save(usuario);
+	        
+	    return ResponseEntity.ok(usuarioSalvo);
 	}
-	
-	/*@PutMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> atualizarcadastro(@RequestBody Usuario usuario){
-		Usuario usuarioSalvo = usuarioRepository.save(usuario);
-		
-		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
-	}*/
 	
 	@PutMapping(value = "/{id}", produces = "application/json")
-	public ResponseEntity<Usuario> atualizarcadastroid(@PathVariable Long id,@RequestBody Usuario usuario){
+	public ResponseEntity<Map<String, Object>> atualizarcadastroid
+				       (@PathVariable Long id, @RequestBody Usuario usuario) {
 		Optional<Usuario> usuariosOptional = usuarioRepository.findById(id);
-		
-		if(!usuariosOptional.isPresent()) {
-			
+		Map<String, Object> response = new HashMap<String, Object>();
+
+		if (!usuariosOptional.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
-		
-		    Usuario usuarioAtualizado = usuarioRepository.save(usuariosOptional.get());
-		    return new ResponseEntity<Usuario>(usuarioAtualizado, HttpStatus.OK);
-	}
-	
-	@PutMapping(value = "/vendausuario/idvenda/{idvenda}", produces = "application/json")
-	public ResponseEntity<Usuario> updadevenda(@PathVariable Long iduser, 
-											   @PathVariable Long idvenda){
-		
-		/*Aqui seria o processo de venda
-		 *Usuario usuarioSalvo = usuarioRepository.save(usuario);*/
-		
-		return new ResponseEntity("Venda Atualizada", HttpStatus.OK);
+
+		if (usuario.getId() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		if (!usuarioRepository.existsById(usuario.getId())) {
+			return ResponseEntity.notFound().build();
+		}
+
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		Usuario usuarioAtualizado = usuarioRepository.save
+				(usuarioRepository.save(usuario));
+
+		response.put("message", "Usuário atualizado com sucesso");
+		response.put("usuario", usuarioAtualizado);
+
+		return ResponseEntity.ok(response);
 	}
 	
 	@DeleteMapping(value = "/{id}", produces = "application/text")
-	public String delete (@PathVariable("id") Long id){
-		
-		usuarioRepository.deleteById(id);
-		
-		return "ok";
+	public ResponseEntity<String> delete(@PathVariable("id") Long id) {
+		try {
+			usuarioRepository.deleteById(id);
+			return ResponseEntity.ok("Exclusão realizada com sucesso!");
+		} catch (EmptyResultDataAccessException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
-	
-
-	@DeleteMapping(value = "/{id}/venda", produces = "application/text")
-	public String deletevenda (@PathVariable("id") Long id){
-		
-		usuarioRepository.deleteById(id);
-		
-		return "ok";
-	}
-	
 }
--- cursospringrestapi/src/main/java/curso/api/rest/repository/UsuarioRepository.java
+++ cursospringrestapi/src/main/java/curso/api/rest/repository/UsuarioRepository.java
@@ -1,13 +1,9 @@
 package curso.api.rest.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import curso.api.rest.model.Usuario;
 
-@Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
 	
-	
-
 }
